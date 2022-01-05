4de8305a720486e4702e7bffbcb91b80bbcbb4a1
Arham Jain <arham.nec@gmail.com>
2022-01-05 13:26:15 +0530

    feat(http): added http client for sdk

---
+++ config.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+type Config struct {
+	AuthKey    string
+	HttpClient *http.Client
+}
+
+type ConfigOptions func(config *Config)
+
+func NewConfigOptions(options ...ConfigOptions) {
+	config := Config{}
+	for _, option := range options {
+		option(&config)
+	}
+	return
+}
--- main.go
+++ main.go
@@ -1,1 +1,5 @@
 package main
+
+type Unmarshal struct {
+
+}
