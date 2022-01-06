94843375ed9252357eedcac90beefc1c43884b3e
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2022-01-06 19:42:12 +0530

    refactor(details,price): moved interface into separate file
    Signed-off-by: Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>

--- pkg/token_price/types.go
+++ pkg/token_price/types.go
@@ -1,17 +1,5 @@
 package token_price
 
-import "github.com/eucrypt/unmarshal-go-sdk/pkg/constants"
-
-type PriceStore interface {
-	GetPriceAtInstant(chain constants.Chain, contractAddress string, timestamp int64) (TokenPrice, error)
-	GetCurrentPrice(chain constants.Chain, contractAddress string) (resp TokenPrice, err error)
-	GetGainers(chain constants.Chain) (resp TokenDetailsResp, err error)
-	GetLosers(chain constants.Chain) (resp TokenDetailsResp, err error)
-	GetLPTokens(chain constants.Chain, lptoken string) (resp TokenListWithPrice, err error)
-	GetTokensPrice(chain constants.Chain, tokenList []string) (resp TokenListWithPrice, err error)
-	GetPriceWithSymbol(symbol string) (resp PriceWithSymbolResp, err error)
-}
-
 type TokenPrice struct {
 	TokenId   string `json:"tokenId"`
 	Timestamp string `json:"timestamp"`
