c1965fc349ec1b0f023e0694fe9d04550ab84b5a
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2022-01-06 15:26:51 +0530

    fix(tokenstore): updated signature in interface
    Signed-off-by: Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>

--- pkg/token_details/types.go
+++ pkg/token_details/types.go
@@ -2,7 +2,7 @@ package token_details
 
 type TokenStore interface {
 	GetTokenDetailsWithContract(contractAddress string) (TokenDetails, error)
-	GetTokenList(queryParams map[string]string) (GetTokenListResponse, error)
+	GetTokenList(pageNumber int, pageSize int) (GetTokenListResponse, error)
 	GetTokenWithSymbol(string) ([]TokenDetails, error)
 }
 
