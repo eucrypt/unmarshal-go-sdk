879e1eb077f9227b70563b463aca9512dac70a6e
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2022-01-11 11:51:54 +0530

    feat(protocols): added protocols type for protocol apis
    Signed-off-by: Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>

---
+++ pkg/constants/protocol.go
@@ -0,0 +1,30 @@
+package constants
+
+import "errors"
+
+type Protocol string
+
+const (
+	PancakeswapV1 Protocol = "pancakeswap_v1"
+	PancakeswapV2 Protocol = "pancakeswap_v2"
+	Uniswap       Protocol = "uniswap"
+	UniswapV2     Protocol = "uniswap_v2"
+)
+
+var allProtocols = map[Protocol]bool{
+	PancakeswapV1: true,
+	PancakeswapV2: true,
+	Uniswap:       true,
+	UniswapV2:     true,
+}
+var allowedCallersByProtocol = map[APIName]map[Protocol]bool{
+	PROTO_GetPairs:     allProtocols,
+	PROTO_GetPositions: allProtocols,
+}
+
+//String returns the string specific version of the protocol
+func (c Protocol) String() string {
+	return string(c)
+}
+
+var UnsupportedProtocolError = errors.New("unsupported protocol for API")
