4164756a669c20497ea653ab40b8ebe199a148d1
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2022-05-05 13:33:06 +0530

    feat(price): adapted to new interface and options

--- pkg/token_price/token_price_impl.go
+++ pkg/token_price/token_price_impl.go
@@ -22,53 +22,38 @@ func New(sess session.Session) PriceStoreImpl {
 // GetTokenPriceAtInstant GetPriceAtInstant accepts a chain, contract address and a timestamp.
 //It fetches the price of a token at a given point in time.
 //If the token is not found, expect an empty response with no error.
-func (p PriceStoreImpl) GetTokenPriceAtInstant(chain constants.Chain, contractAddress string, timestamp int64) (resp types.TokenPrice, err error) {
+func (p PriceStoreImpl) GetTokenPrice(chain constants.Chain, contractAddress string, options *types.GetPriceOptions) (resp types.TokenPrice, err error) {
+	fmt.Println("Getting token price")
 	if !constants.PS_GetTokensPrice.SupportsChain(chain) {
 		return types.TokenPrice{}, constants.UnsupportedChainError
 	}
 	path := strings.Replace(constants.PS_GetPriceWithAddress.GetURI(), ":chain", chain.String(), 1)
 	path = strings.Replace(path, ":address", contractAddress, 1)
-	var queryParams = map[string]interface{}{
-		"timestamp": fmt.Sprint(timestamp),
-	}
-	var urlVals = httpclient.QueryParamHelper(queryParams)
+	var urlVals = p.getTokenPriceParams(options)
+	fmt.Println("Got query params")
 	err = p.sess.Client.Get(&resp, path, urlVals)
-	return
-}
-
-// GetTokenCurrentPrice GetCurrentPrice accepts a chain and a contract address. It fetches the token's current price.
-//If the token is not found, expect an empty response with no error.
-func (p PriceStoreImpl) GetTokenCurrentPrice(chain constants.Chain, contractAddress string) (resp types.TokenPrice, err error) {
-	if !constants.PS_GetTokensPrice.SupportsChain(chain) {
-		return types.TokenPrice{}, constants.UnsupportedChainError
-	}
-	path := strings.Replace(constants.PS_GetPriceWithAddress.GetURI(), ":chain", chain.String(), 1)
-	path = strings.Replace(path, ":address", contractAddress, 1)
-	var urlVals = make(url.Values)
-	err = p.sess.Client.Get(&resp, path, urlVals)
-
 	return
 }
 
 //GetTopGainers accepts only the chain and returns a list of top gainers
-func (p PriceStoreImpl) GetTopGainers(chain constants.Chain) (resp types.TokenDetailsResp, err error) {
+func (p PriceStoreImpl) GetTopGainers(chain constants.Chain, options *types.GetTopGainersOptions) (resp types.TokenDetailsResp, err error) {
 	if !constants.PS_GetGainers.SupportsChain(chain) {
 		return types.TokenDetailsResp{}, constants.UnsupportedChainError
 	}
 	path := strings.Replace(constants.PS_GetGainers.GetURI(), ":chain", chain.String(), 1)
-	var urlVals = make(url.Values)
+	var urlVals = p.getGainersOptions(options)
 	err = p.sess.Client.Get(&resp, path, urlVals)
 
 	return
 }
 
 //GetTopLosers accepts only the chain and returns a list of top losers for that chain
-func (p PriceStoreImpl) GetTopLosers(chain constants.Chain) (resp types.TokenDetailsResp, err error) {
+func (p PriceStoreImpl) GetTopLosers(chain constants.Chain, options *types.GetTopLosersOptions) (resp types.TokenDetailsResp, err error) {
 	if !constants.PS_GetLosers.SupportsChain(chain) {
 		return types.TokenDetailsResp{}, constants.UnsupportedChainError
 	}
 	path := strings.Replace(constants.PS_GetLosers.GetURI(), ":chain", chain.String(), 1)
-	var urlVals = make(url.Values)
+	var urlVals = p.getLosersOptions(options)
 	err = p.sess.Client.Get(&resp, path, urlVals)
 
 	return
@@ -106,10 +91,55 @@ func (p PriceStoreImpl) GetMultipleTokenPrice(chain constants.Chain, tokenList [
 
 //GetTokenPriceBySymbol accepts a Symbol and returns an array of token with their prices that match the symbol
 //If the token is not found, expect an empty response with no error.
-func (p PriceStoreImpl) GetTokenPriceBySymbol(symbol string) (resp types.PriceWithSymbolResp, err error) {
+func (p PriceStoreImpl) GetTokenPriceBySymbol(symbol string, options *types.GetPriceWithSymbolOptions) (resp types.PriceWithSymbolResp, err error) {
 	path := strings.Replace(constants.PS_GetPriceBySymbol.GetURI(), ":symbol", symbol, 1)
-	var urlVals = make(url.Values)
+	var urlVals = p.getTokenPriceBySymbolParams(options)
 	err = p.sess.Client.Get(&resp, path, urlVals)
 
 	return
+}
+
+func (p PriceStoreImpl) getTokenPriceParams(options *types.GetPriceOptions) url.Values {
+	if options == nil {
+		return make(url.Values)
+	}
+	var queryStrings = make(map[string]interface{})
+	if options.Timestamp != 0 {
+		queryStrings["timestamp"] = options.Timestamp
+	}
+	queryStrings["24hchange"] = options.TwentyFourHourChange
+	queryStrings["alternateChain"] = options.AlternateChain
+
+	return httpclient.QueryParamHelper(queryStrings)
+}
+
+func (p PriceStoreImpl) getGainersOptions(options *types.GetTopGainersOptions) url.Values {
+	if options == nil {
+		return make(url.Values)
+	}
+	var queryStrings = make(map[string]interface{})
+
+	queryStrings["price"] = options.MinimumPrice
+	return httpclient.QueryParamHelper(queryStrings)
+}
+
+func (p PriceStoreImpl) getLosersOptions(options *types.GetTopLosersOptions) url.Values {
+	if options == nil {
+		return make(url.Values)
+	}
+	var queryStrings = make(map[string]interface{})
+
+	queryStrings["price"] = options.MinimumPrice
+	return httpclient.QueryParamHelper(queryStrings)
+}
+
+func (p PriceStoreImpl) getTokenPriceBySymbolParams(options *types.GetPriceWithSymbolOptions) url.Values {
+	if options == nil {
+		return make(url.Values)
+	}
+	var queryStrings = make(map[string]interface{})
+	if options.Timestamp != 0 {
+		queryStrings["timestamp"] = options.Timestamp
+	}
+	return httpclient.QueryParamHelper(queryStrings)
 }
