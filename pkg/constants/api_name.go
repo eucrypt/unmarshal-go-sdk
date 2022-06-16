bcd36b7eed321ab0b07deb2abe53e24beabbb9c8
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2022-06-16 14:02:31 +0530

    add(chain, api): added additionally supported chains and api

--- pkg/constants/api_name.go
+++ pkg/constants/api_name.go
@@ -4,43 +4,45 @@ type APIName string
 
 //goland:noinspection GoSnakeCaseUsage
 const (
-	PS_GetPriceWithAddress   APIName = "v1/pricestore/chain/:chain/:address"
-	PS_GetTokensPrice        APIName = "v1/pricestore/chain/:chain/tokens"
-	PS_GetLpTokenPrice       APIName = "v1/pricestore/chain/:chain/lptokens"
-	PS_GetLosers             APIName = "v1/pricestore/chain/:chain/losers"
-	PS_GetGainers            APIName = "v1/pricestore/chain/:chain/gainers"
-	PS_GetPriceBySymbol      APIName = "v1/pricestore/:symbol"
-	TS_GetDetailsByContract  APIName = "v1/tokenstore/token/address/:address"
-	TS_GetTokenList          APIName = "v1/tokenstore/token/all"
-	TS_GetTokenBySymbol      APIName = "v1/tokenstore/token/symbol/:symbol"
-	ASSETS_GetTokenAssets    APIName = "v1/:chain/address/:address/assets"
-	ASSETS_GetProfitsAndLoss APIName = "v2/:chain/address/:address/userData"
-	NFT_GetNFTAssets         APIName = "v1/:chain/address/:address/nft-assets"
-	NFT_GetTxns              APIName = "v1/:chain/address/:address/nft-transactions"
-	NFT_GetDetailsByID       APIName = "v1/:chain/address/:address/details"
-	NFT_GetHoldersByID       APIName = "v1/:chain/address/:address/nftholders"
-	TXN_GetTokenTxns         APIName = "v1/:chain/address/:address/transactions"
-	TXN_GetTxnDetails        APIName = "v1/:chain/transactions/:txnID"
-	TXN_GetTokenTxnsV2       APIName = "v2/:chain/address/:address/transactions"
-	PROTO_GetPositions       APIName = "v2/protocols/:protocol/address/:address/positions"
-	PROTO_GetPairs           APIName = "v2/protocols/:protocol/pairs"
+	PS_GetPriceWithAddress       APIName = "v1/pricestore/chain/:chain/:address"
+	PS_GetTokensPrice            APIName = "v1/pricestore/chain/:chain/tokens"
+	PS_GetLpTokenPrice           APIName = "v1/pricestore/chain/:chain/lptokens"
+	PS_GetLosers                 APIName = "v1/pricestore/chain/:chain/losers"
+	PS_GetGainers                APIName = "v1/pricestore/chain/:chain/gainers"
+	PS_GetPriceBySymbol          APIName = "v1/pricestore/:symbol"
+	TS_GetDetailsByContract      APIName = "v1/tokenstore/token/address/:address"
+	TS_GetTokenList              APIName = "v1/tokenstore/token/all"
+	TS_GetTokenBySymbol          APIName = "v1/tokenstore/token/symbol/:symbol"
+	ASSETS_GetTokenAssets        APIName = "v1/:chain/address/:address/assets"
+	ASSETS_GetProfitsAndLoss     APIName = "v2/:chain/address/:address/userData"
+	NFT_GetNFTAssets             APIName = "v1/:chain/address/:address/nft-assets"
+	NFT_GetTxns                  APIName = "v1/:chain/address/:address/nft-transactions"
+	NFT_GetDetailsByID           APIName = "v1/:chain/address/:address/details"
+	NFT_GetHoldersByID           APIName = "v1/:chain/address/:address/nftholders"
+	TXN_GetTokenTxns             APIName = "v1/:chain/address/:address/transactions"
+	TXN_GetTxnDetails            APIName = "v1/:chain/transactions/:txnID"
+	TXN_GetTokenTxnsV2           APIName = "v2/:chain/address/:address/transactions"
+	TXN_GetRawTransactionDetails APIName = "/v1/:chain/address/:address/transactions/raw"
+	PROTO_GetPositions           APIName = "v2/protocols/:protocol/address/:address/positions"
+	PROTO_GetPairs               APIName = "v2/protocols/:protocol/pairs"
 )
 
 var allowedCallersByChain = map[APIName]map[Chain]bool{
-	PS_GetPriceWithAddress:   priceStoreSupportedWithAvax,
-	PS_GetTokensPrice:        priceStoreSupportedWithAvax,
-	PS_GetLpTokenPrice:       priceStoreSupportedWithAvax,
-	PS_GetLosers:             priceStoreSupported,
-	PS_GetGainers:            priceStoreSupported,
-	ASSETS_GetTokenAssets:    allChains,
-	ASSETS_GetProfitsAndLoss: priceStoreSupported,
-	NFT_GetNFTAssets:         {ETH: true, BSC: true, MATIC: true, AVALANCHE: true, SOL: true},
-	NFT_GetTxns:              nftEVMSupport,
-	NFT_GetDetailsByID:       nftEVMSupport,
-	NFT_GetHoldersByID:       nftEVMSupport,
-	TXN_GetTokenTxns:         {ETH: true, BSC: true, MATIC: true, SOL: true, ZILLIQA: true, AVALANCHE: true, XDC: true},
-	TXN_GetTxnDetails:        {ETH: true, BSC: true, MATIC: true, SOL: true, AVALANCHE: true, XDC: true},
-	TXN_GetTokenTxnsV2:       {ETH: true, BSC: true, MATIC: true, AVALANCHE: true, XDC: true},
+	PS_GetPriceWithAddress:       priceStoreSupportedWithAvax,
+	PS_GetTokensPrice:            priceStoreSupportedWithAvax,
+	PS_GetLpTokenPrice:           priceStoreSupportedWithAvax,
+	PS_GetLosers:                 priceStoreSupported,
+	PS_GetGainers:                priceStoreSupported,
+	ASSETS_GetTokenAssets:        allChains,
+	ASSETS_GetProfitsAndLoss:     priceStoreSupported,
+	NFT_GetNFTAssets:             {ETH: true, BSC: true, MATIC: true, AVALANCHE: true, SOL: true},
+	NFT_GetTxns:                  nftEVMSupport,
+	NFT_GetDetailsByID:           nftEVMSupport,
+	NFT_GetHoldersByID:           nftEVMSupport,
+	TXN_GetTokenTxns:             {ETH: true, BSC: true, MATIC: true, SOL: true, ZILLIQA: true, AVALANCHE: true, XDC: true},
+	TXN_GetTxnDetails:            {ETH: true, BSC: true, MATIC: true, SOL: true, AVALANCHE: true, XDC: true},
+	TXN_GetTokenTxnsV2:           {ETH: true, BSC: true, MATIC: true, AVALANCHE: true, XDC: true},
+	TXN_GetRawTransactionDetails: rawTxnSupported,
 }
 
 //SupportsChain Allows a caller to know if a chain specific API supports a passed valid chain
