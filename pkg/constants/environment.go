908cdb9d61aab6c4b8fb2e03ebda682aa5aeac24
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2022-11-24 17:40:02 +0530

    fix(environment): Updated production endpoint

--- pkg/constants/environment.go
+++ pkg/constants/environment.go
@@ -3,15 +3,13 @@ package constants
 type Environment string
 
 const (
-	Prod     Environment = "prod"
-	Stage                = "stage"
-	Internal             = "internal" //Internal only for use within Unmarshal. Will not work otherwise.
+	Prod  Environment = "prod"
+	Stage             = "stage"
 )
 
 var endpointMap = map[Environment]string{
-	Prod:     "https://api.unmarshal.com",
-	Stage:    "https://stg-api.unmarshal.com",
-	Internal: "https://api.prod.unmarshal.com",
+	Prod:  "https://api.prod.unmarshal.com",
+	Stage: "https://stg-api.unmarshal.com",
 }
 
 func (e Environment) GetEndpoint() string {
