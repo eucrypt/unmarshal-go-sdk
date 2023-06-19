471d69c412f06695991f2934324b111c48810013
Anusha Sridhar <anusha@unmarshal.io>
2023-06-19 17:15:37 +0530

    Feat(chains): Integrate Mantle testnet (#35)
    * Feat(chains): Integrate Mantle testnet
    
    * Rename MantleTestnet to MANTLE_TESTNET

--- pkg/constants/chain.go
+++ pkg/constants/chain.go
@@ -8,55 +8,57 @@ type Chain string
 
 //goland:noinspection GoSnakeCaseUsage
 const (
-	ARBITRUM      Chain = "arbitrum"
-	AVALANCHE     Chain = "avalanche"
-	BSC           Chain = "bsc"
-	BSC_TESTNET   Chain = "bsc-testnet"
-	CELO          Chain = "celo"
-	CRONOS        Chain = "cronos"
-	ETH           Chain = "ethereum"
-	ETH_RINKEBY   Chain = "rinkeby-testnet"
-	FANTOM        Chain = "fantom"
-	FUSE          Chain = "fuse"
-	HUOBI         Chain = "heco"
-	KLAYTN        Chain = "klaytn"
-	MATIC         Chain = "matic"
-	MATIC_TESTNET Chain = "matic-testnet"
-	OPTIMISM      Chain = "optimism"
-	SOL           Chain = "solana"
-	VELAS         Chain = "velas"
-	XDC           Chain = "xinfin"
-	ZILLIQA       Chain = "zilliqa"
-	MOONBEAM      Chain = "moonbeam"
-	METIS         Chain = "metis"
-	AURORA        Chain = "aurora"
-	MaticSupernet Chain = "matic-supernet"
-	ZKEVM         Chain = "zkevm"
+	ARBITRUM       Chain = "arbitrum"
+	AVALANCHE      Chain = "avalanche"
+	BSC            Chain = "bsc"
+	BSC_TESTNET    Chain = "bsc-testnet"
+	CELO           Chain = "celo"
+	CRONOS         Chain = "cronos"
+	ETH            Chain = "ethereum"
+	ETH_RINKEBY    Chain = "rinkeby-testnet"
+	FANTOM         Chain = "fantom"
+	FUSE           Chain = "fuse"
+	HUOBI          Chain = "heco"
+	KLAYTN         Chain = "klaytn"
+	MATIC          Chain = "matic"
+	MATIC_TESTNET  Chain = "matic-testnet"
+	OPTIMISM       Chain = "optimism"
+	SOL            Chain = "solana"
+	VELAS          Chain = "velas"
+	XDC            Chain = "xinfin"
+	ZILLIQA        Chain = "zilliqa"
+	MOONBEAM       Chain = "moonbeam"
+	METIS          Chain = "metis"
+	AURORA         Chain = "aurora"
+	MaticSupernet  Chain = "matic-supernet"
+	ZKEVM          Chain = "zkevm"
+	MANTLE_TESTNET Chain = "mantle-testnet"
 )
 
 // This should be manually changed when a new chain starts being supported
 var allChains = map[Chain]bool{
-	ARBITRUM:      true,
-	AVALANCHE:     true,
-	BSC:           true,
-	CELO:          true,
-	CRONOS:        true,
-	ETH:           true,
-	FANTOM:        true,
-	FUSE:          true,
-	HUOBI:         true,
-	KLAYTN:        true,
-	MATIC:         true,
-	OPTIMISM:      true,
-	SOL:           true,
-	VELAS:         true,
-	XDC:           true,
-	ZILLIQA:       true,
-	MOONBEAM:      true,
-	METIS:         true,
-	AURORA:        true,
-	MaticSupernet: true,
-	ZKEVM:         true,
+	ARBITRUM:       true,
+	AVALANCHE:      true,
+	BSC:            true,
+	CELO:           true,
+	CRONOS:         true,
+	ETH:            true,
+	FANTOM:         true,
+	FUSE:           true,
+	HUOBI:          true,
+	KLAYTN:         true,
+	MATIC:          true,
+	OPTIMISM:       true,
+	SOL:            true,
+	VELAS:          true,
+	XDC:            true,
+	ZILLIQA:        true,
+	MOONBEAM:       true,
+	METIS:          true,
+	AURORA:         true,
+	MaticSupernet:  true,
+	ZKEVM:          true,
+	MANTLE_TESTNET: true,
 }
 
 var priceStoreSupported = map[Chain]bool{
@@ -68,7 +70,8 @@ var priceStoreSupported = map[Chain]bool{
 
 var rawTxnSupported = map[Chain]bool{ETH: true, ETH_RINKEBY: true, BSC: true, BSC_TESTNET: true, MATIC: true,
 	MATIC_TESTNET: true, OPTIMISM: true, AVALANCHE: true, ARBITRUM: true, CELO: true, FANTOM: true, KLAYTN: true,
-	FUSE: true, CRONOS: true, VELAS: true, MOONBEAM: true, METIS: true, AURORA: true, MaticSupernet: true, ZKEVM: true}
+	FUSE: true, CRONOS: true, VELAS: true, MOONBEAM: true, METIS: true, AURORA: true, MaticSupernet: true, ZKEVM: true,
+	MANTLE_TESTNET: true}
 
 var nftEVMSupport = map[Chain]bool{ETH: true, BSC: true, MATIC: true, AVALANCHE: true, VELAS: true, KLAYTN: true, FUSE: true, CRONOS: true}
 
